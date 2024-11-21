60557be800b913c05dccf03cb9e19d59e939df85
Ryan Zhao <105570693+Rymas-robcv@users.noreply.github.com>
2024-11-20 22:47:50 -0500

    Update AI trading bot.py

--- AI trading bot.py
+++ AI trading bot.py
@@ -11,5 +11,18 @@ warnings.filterwarnings("ignore")
 
 sp500 = pd.read.html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
 
-sp500["Symbol"] = sp500["symbol"].str.replace(".","-")
+for i in range(len(sp500)):
+    if "." in sp500[i]:
+        sp500[i].str.replace(".","-")
 
+
+symbolsList = sp500["Symbol"].unique().tolist()
+
+end_date = "2024-11-16"
+start_date = pd.to_datetime(end_date)-pd.DateOffset(365*8)
+df = yf.download(tickers = symbolsList,
+                 start =start_date,
+                 end = end_date)
+df.index.names = ['date','ricker']
+
+df.columns = df.columns.str.Lower()
