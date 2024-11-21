d14b1e9513005118fd2eb731385d568ac68be4b7
Ryan Zhao <105570693+Rymas-robcv@users.noreply.github.com>
2024-11-20 22:50:48 -0500

    Created new Twitter sentiment trading bot

---
+++ Twitter sentiment trading strategy.py
@@ -0,0 +1,9 @@
+import os
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import datetime as dt
+import yfinance as yf
+
+plt.style.use('ggplot')
